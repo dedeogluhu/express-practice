1231d941868544bfdb0b972c7b2be1ea5bd0fdae
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-12 19:53:01 +0300

    Cleanup on app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { PORT } = require('./config');
+const { connectMongoDB } = require('./utils');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+const listRouter = require('./routers/api/list');
+const addRouter = require('./routers/api/add');
+
+app.use('/', listRouter);
+app.use('/add', addRouter);
+
+connectMongoDB();
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
